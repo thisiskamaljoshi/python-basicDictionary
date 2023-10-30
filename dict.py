fe9460b1d846ae634d44d1b9c7540ffdd3d451d5
thisiskamaljoshi <thisiskamaljoshi@gmail.com>
2023-10-30 15:28:54 +0530

    if the word does not exist we will find the most similar word from our dictionary

--- dict.py
+++ dict.py
@@ -1,15 +1,24 @@
 import json
-
+import difflib
+from difflib import get_close_matches
 data = json.load(open("dict_data.json"))
 
 def meaning(word):
-    word=word.lower()
-    if(word in data):
-        return data[word]
-    else:
-        return ["Word Doesnt Exist In Dictionary"]
+    word_lowercase=word.lower()
 
-word =input("Enter Word:")
+    if(word_lowercase in data):
+        return data[word_lowercase]
+    else:
+        closestWord = get_close_matches(word_lowercase,data.keys(),n=1,cutoff=0.8)
+        wordCheck="n"
+        if(len(closestWord) >0):
+            wordCheck= input("Cannot find {}. \nDid you mean {} instead? (Y/N)  ".format(word,closestWord[0])).lower()
+        if(wordCheck == "y"):
+            return data[closestWord[0]]
+        else:    
+            return ["The word doesnt exist in dictionary"]
+
+word =input("Enter Word: ")
 
 meanings= meaning(word)
 
