9fce33666b6fbf132707fed6e4036b19420bab51
thisiskamaljoshi <thisiskamaljoshi@gmail.com>
2023-10-30 14:14:28 +0530

    made input case insensitive

--- dict.py
+++ dict.py
@@ -3,12 +3,15 @@ import json
 data = json.load(open("dict_data.json"))
 
 def meaning(word):
+    word=word.lower()
     if(word in data):
         return data[word]
     else:
-        return ["No Word Found"]
+        return ["Word Doesnt Exist In Dictionary"]
 
-meanings =meaning("rn")
+word =input("Enter Word:")
+
+meanings= meaning(word)
 
 for meaning in meanings:
     print(meaning)
